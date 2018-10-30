7de7ab19fec2dd8450b425811ac14b672424bb40
ArianaMikel <42385375+ArianaMikel@users.noreply.github.com>
2018-10-30 09:46:00 -0700

    10.30 added loops

--- notes/Ariana Mikel - Mad Lib.py
+++ notes/Ariana Mikel - Mad Lib.py
@@ -10,10 +10,11 @@ name9 = input("Enter a number")
 name10 = input("Enter a noun (plural)")
 name11 = input("Enter a verb")
 name12 = input("Enter a number")
+
+
 print("In this passage of the %s, he and his loyal friends were caught alone with a %s. " % (name,name2))
 print("It was a %s, %s, and a %s." % (name3, name4, name5))
 print("Not knowing how to get out alive %s came up with a plan." % name)
 print("%s and his loyal friends would %s the %s with a %s." % (name, name6, name2, name8))
 print("After they defeated the %s, %s, %s more came at the team they " % (name2, name9, name10))
 print("%s for %s hours until they defeated all the %s" % (name11, name12, name10))
-
