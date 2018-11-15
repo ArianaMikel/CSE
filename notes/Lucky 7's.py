d84b024403cad16c7f57c796ec82ee73dbb3e3a1
ArianaMikel <42385375+ArianaMikel@users.noreply.github.com>
2018-11-15 09:46:12 -0800

    11.15.18

--- notes/Lucky 7's.py
+++ notes/Lucky 7's.py
@@ -1,6 +1,10 @@
 import random
-dice1 = random.randint(1, 6)
-dice2 = random.randint(1, 6)
-while dice1 + dice2 == 7:
+cash = 15
+rounds = 0
+while cash > 0
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
+    if dice1 + dice2 == 7:
+        cash + 4
+        rounds = rounds - 1
+        
