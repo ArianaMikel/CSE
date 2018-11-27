873cc5f3c671e13d55f9f7f48a12b5eea7c625be
ArianaMikel <42385375+ArianaMikel@users.noreply.github.com>
2018-11-27 09:48:05 -0800

    Hangman Notes

--- notes/Lists Notes.py
+++ notes/Lists Notes.py
@@ -20,7 +20,7 @@ for item in colors:
 2. change the 3rd thing in the list
 3. print the item
 4. print the full list
-'''
+
 
 new_list = ["work", "school", "home", "supermarket", "restaurant", "beach", "prison"]
 new_list[2] = "desert"
@@ -32,3 +32,66 @@ print(new_list[1:3])
 print(new_list[1:4])
 print(new_list[1:])
 print(new_list[:6])
+'''
+
+
+
+food_list = ["pizza", "enchiladas", "popcorn", "chicken", "cupcake", "cake", "cookies", "rice","noodles", "meatball",
+             "clam chowder", "hamburger","chicken wings","salad", "french fries","salmon", "alfredo","garlic bread",
+             "shrimp", "carne asada", "chili", "bread"]
+print(len(food_list))
+
+# Adding stuff to a list
+food_list.append("bacon")
+food_list.append("eggs")
+# Notice that everything is object.method(parameters)
+print(food_list)
+food_list.insert(1, "eggo waffles")
+print(food_list)
+
+# Removing things from a list
+food_list.remove("salad")
+print(food_list)
+
+'''
+ari_list = ["pretzels", "pizza", "pie"]
+print(ari_list)
+ari_list.append("pecan")
+print(ari_list)
+ari_list.remove("pretzels")
+print(ari_list)
+
+# Tuples
+brands = ("apple", "samsung", "HTC") # Noctice the parentheses, this is a tuple it cannot be changed, 
+#it is immutable, not a list
+'''
+
+# Also removing stuff from a LIST
+print(food_list)
+food_list.pop(0)
+print(food_list)
+
+
+# Find the index of an item
+print(food_list.index("chicken"))
+
+# Changing things into a list, extremely handy para hangman
+string1 = "turquoise"
+list1 = list(string1)
+print(list1)
+
+# Turn a list into a string
+print("".join(list1))
+
+for i in range (len(list1)):  # i goes through all indicies
+    if list1[i] == "u":   # if we find a U
+        list.pop(i)   # remove the i-th index
+        list.insert(i, "*")   # Put a * there instead
+'''
+for character in list1:
+    is character == "u":
+    #   replace with a *
+    current_index = list1.index(character)
+    list1.pop(current_index)
+    list1.insert(current_index, "*")
+'''
