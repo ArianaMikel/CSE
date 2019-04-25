ac66047426215e6bccf7594ade8e60e475a8d9df
ArianaMikel <42385375+ArianaMikel@users.noreply.github.com>
2019-04-25 09:48:06 -0700

    New LAptopos

---
+++ notes/CSVNotes.py
@@ -0,0 +1,27 @@
+import csv
+
+# with open("Book1.csv", 'r') as old_csv:
+#     with open("myNewFile.csv", 'w', newline='') as new_csv:
+#         print("Writing file...")
+#         reader = csv.reader(old_csv)
+#         writer = csv.writer(new_csv)
+#         for row in reader:
+#             old_number = int(row[0])
+#             new_number = old_number + 1
+#             row[0] = new_number
+#             writer.writerow(row)
+        # old_number = int(row[0]) + 1
+        # print(old_number)
+        with open("Book1.csv", 'r') as old_csv:
+            with open("myNewFile.csv", 'w', newline='') as new_csv:
+                print("Writing file...")
+                reader = csv.reader(old_csv)
+                writer = csv.writer(new_csv)
+                for row in reader:
+                    old_number = row[0]
+                    first_num = int(old_number[0])
+                    if first_num % 2 == 0:
+                        writer.writerow(row)
+                    # print(int(old_number) + 1)
+                    # print(old_number)
+print("OK")
