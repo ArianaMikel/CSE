ca364c6a4cd5712e85df8d2a2383ec609ca15ac5
ArianaMikel <42385375+ArianaMikel@users.noreply.github.com>
2019-05-09 09:47:11 -0700

    5/9/19

--- notes/Ariana Mikel- Sales Record.py
+++ notes/Ariana Mikel- Sales Record.py
@@ -1,4 +1,15 @@
 import csv
 
-def print_item_type():
-    print("")
+def get_total():
+    fruits = 0
+    clothes = 0
+    meat = 0
+    beverages = 0
+    office_supplies = 0
+    cosmetics = 0
+    personal_care = 0
+    household = 0
+    baby_food = 0
+    vegetables = 0
+    snacks = 0
+    cereal = 0
