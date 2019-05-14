9abc6e76caf785c03d0267babe18a6703d23d36c
ArianaMikel <42385375+ArianaMikel@users.noreply.github.com>
2019-05-14 09:47:05 -0700

    NO GANNON

--- notes/Ariana Mikel- Sales Record.py
+++ notes/Ariana Mikel- Sales Record.py
@@ -17,5 +17,71 @@ with open("Sales Records(1).csv", 'r') as old_csv:
     cereal = 0
     print("Processing...")
     for row in reader:
-        if row[2] == "Fruits"
+        if row[2] == "Fruits":
+            profits = float(row[13])
+            for i in row[13]:
+                fruits += profits
+        elif row[2] == "Clothes":
+            profits = float(row[13])
+            for i in row[13]:
+                clothes += profits
+        elif row[2] == "Meats":
+            profits = float(row[13])
+            for i in row[13]:
+                meat += profits
+        elif row[2] == "Beverages":
+            profits = float(row[13])
+            for i in row[13]:
+                beverages += profits
+        elif row[2] == "Office Supplies":
+            profits = float(row[13])
+            for i in row[13]:
+                office_supplies += profits
+        elif row[2] == "Cosmetics":
+            profits = float(row[13])
+            for i in row[13]:
+                cosmetics += profits
+        elif row[2] == "Person Care":
+            profits = float(row[13])
+            for i in row[13]:
+                personal_care += profits
+        elif row[2] == "Household":
+            profits = float(row[13])
+            for i in row[13]:
+                household += profits
+        elif row[2] == "Baby Food":
+            profits = float(row[13])
+            for i in row[13]:
+                baby_food += profits
+        elif row[2] == "Vegetables":
+            profits = float(row[13])
+            for i in row[13]:
+                vegetables += profits
+        elif row[2] == "Snacks":
+            profits = float(row[13])
+            for i in row[13]:
+                snacks += profits
+        elif row[2] == "Cereal":
+            profits = float(row[13])
+            for i in row[13]:
+                cereal += profits
 
+the_profits = [fruits, clothes, meat, beverages, office_supplies, cosmetics, personal_care, household,
+               baby_food, vegetables, snacks, cereal]
+
+print("The profit for fruits is %d" % fruits)
+print("The profit for clothes is %d" % clothes)
+print("The profit for meat is %d" % meat)
+print("The profit for beverages is %d" % beverages)
+print("The profit for office supplies is %d" % office_supplies)
+print("The profit for cosmetics is %d" % cosmetics)
+print("The profit for personal care is %d" % personal_care)
+print("The profit for household is %d" % household)
+print("The profit for baby food is %d" % baby_food)
+print("The profit for vegetables is %d" % vegetables)
+print("The profit for snacks is %d" % snacks)
+print("The profit for cereal is %d" % cereal)
+
+
+if max(the_profits) == fruits:
+    print("FD")
