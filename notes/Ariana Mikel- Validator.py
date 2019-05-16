f309bfdd9b98ecb535a5efeb4aecebd0c8347872
ArianaMikel <42385375+ArianaMikel@users.noreply.github.com>
2019-05-16 09:48:12 -0700

    yEA

--- notes/Ariana Mikel- Validator.py
+++ notes/Ariana Mikel- Validator.py
@@ -26,7 +26,19 @@ def valid_card_number(num: str):
         return False
     valid_card_number_list = list(num)
     valid_card_number_list.pop(15)
-    for number in range(len(num))
+    for number in range(len(valid_card_number_list)):
+        valid_card_number_list[number] = int(valid_card_number_list[number])
+    reversed_num = num[::-1]
+    reversed_num_list = []
+    for i in range (len(reversed_num)):
+        reversed_num_list.append(int(reversed_num[i]))
+    for index in range(len(reversed_num_list)):
+        if divisible_by_2(index):
+            if int(reversed_num_list[index]) * 2 > 9:
+                reversed_num_list[index] = int(reversed_num_list[index]) * 2 - 9
+            else: 
+
+
 
 def reverse(string):
     print(string[::-1])
